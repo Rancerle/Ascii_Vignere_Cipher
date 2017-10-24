992abfaa4a27082e932927e7ed3e562e101f1fbf
Reilly Q. Curran <reillycurran880@gmail.com>
2017-10-24 09:34:55 -0400

    Added additional test code
    Additional test code to try defining things by the ".length" function.  Doing so does print everything out as intended and return the length of the array on one side.

--- src/Test_Ascii.java
+++ src/Test_Ascii.java
@@ -3,15 +3,15 @@ public class Test_Ascii {
 
 	public static void main(String[] args) 
 	{
-		for(int i = 0; i < 92; i++)
+		for(int i = 0; i < Ascii_Vigner_Cipher.VIGNERE.length; i++)
 		{
-			for(int j = 0; j < 92; j++)
+			for(int j = 0; j < Ascii_Vigner_Cipher.VIGNERE.length; j++)
 			{
 				System.out.print(Ascii_Vigner_Cipher.VIGNERE[i][j] + " ");
 			}
 			System.out.print('\n');
 		}
-
+		System.out.print(Ascii_Vigner_Cipher.VIGNERE.length);
 	}
 
 }
